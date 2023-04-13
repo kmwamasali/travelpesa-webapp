2047fe69320b403e99a631539a6199b544fca9d3
kmwamasali <kmwamasali@gmail.com>
2023-04-13 23:33:33 +0300

    BUGFIX: fix image and link lint errors

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,10 @@
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Login() {
     return (
         <main className={styles.main}>
-            <a href="/api/auth/login">Login</a>
+            <Link href="/api/auth/login">Login</Link>
         </main>
     );
 }
